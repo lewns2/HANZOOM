3b47e7e1ee9f64813492de3bb1df2824e6da6927
김동주 <rlaehdwn23@naver.com>
2022-04-18 23:53:07 +0900

    add : designed signup page
    - 회원가입 페이지 디자인
    - 회원가입 페이지 라우팅 설정
    - package.json 필요 라이브러리 추가

--- frontend/src/pages/Routing.jsx
+++ frontend/src/pages/Routing.jsx
@@ -2,22 +2,24 @@ import { Main } from './Main';
 import { Board } from './Board';
 import { MyFoodIngredients } from './MyFoodIngredients';
 import { MyPage } from './MyPage';
+import { SignUp } from './User/SignUp';
 
-import {Navbar} from '../components/Navbar';
+import { Navbar } from '../components/Navbar';
 
 import { Route, Routes, Navigate, BrowserRouter } from 'react-router-dom';
 
 export const Routing = () => {
-    return (
-      <BrowserRouter>
-        <Navbar/>
-        <Routes>
-          <Route path="/main" element={<Main />} />
-          <Route path="/board" element={<Board />} />
-          <Route path="/my-food-ingredients" element={<MyFoodIngredients />} />
-          <Route path="/my-page" element={<MyPage />} />
-          <Route path="*" element={<Navigate replace to="/main" />} />
-        </Routes>
-      </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/main" element={<Main />} />
+        <Route path="/board" element={<Board />} />
+        <Route path="/my-food-ingredients" element={<MyFoodIngredients />} />
+        <Route path="/my-page" element={<MyPage />} />
+        <Route path="*" element={<Navigate replace to="/main" />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
