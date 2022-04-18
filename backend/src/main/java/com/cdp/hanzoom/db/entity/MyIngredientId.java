e8ca3cfa7c94a10c257419622ec568a3091a1676
한성희 <bee1404@naver.com>
2022-04-18 15:30:45 +0900

    add : ingredient, myingredient entity
    - 식재료 및 MY식재료 테이블 생성

---
+++ backend/src/main/java/com/cdp/hanzoom/db/entity/MyIngredientId.java
@@ -0,0 +1,25 @@
+package com.cdp.hanzoom.db.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class MyIngredientId implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name="user_nickname")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User userNickname;
+
+    @OneToOne
+    @JoinColumn(name = "ingredient_no")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Ingredient ingredientNo;
+}
