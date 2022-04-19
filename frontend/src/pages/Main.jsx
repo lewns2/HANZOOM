ef6f001a8eeaabff623e24aa81caadb9fb63318e
김동주 <rlaehdwn23@naver.com>
2022-04-20 02:02:14 +0900

    add : user information in redux store
    - 로그인, 로그아웃 시, 유저 정보 store에 저장, 제거
    - redux toolkit 설정
    - store, userSlice 파일 추가

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,7 +1,14 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
 export const Main = () => {
-    return (
-        <>
-            <h1>시작 페이지</h1>
-        </>
-    );
+  // redux user 테스트
+  //   const user = useSelector((state) => state.user);
+
+  return (
+    <>
+      {/* {console.log(user)} */}
+      <h1>시작 페이지</h1>
+    </>
+  );
 };
