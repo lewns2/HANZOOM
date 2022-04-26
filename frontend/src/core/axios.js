d7e70634fc22ba4525e0ee56a2f0f4e56e63c3ef
김동주 <rlaehdwn23@naver.com>
2022-04-26 11:33:36 +0900

    update : navbar 기능
    - 로그인 유무에 따른 navbar 표시
    - navbar 이동 시 페이지 전체 랜더링 현상 해결

--- frontend/src/core/axios.js
+++ frontend/src/core/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'http://localhost:8443/api';
+export const API_URL = 'https://localhost:8443/api';
 
 export const Axios = axios.create({
   baseURL: API_URL,
