f95b760b35ef13a4f3a013bfd56550b6bd70b1e5
zlewns <zlewns@gmail.com>
2022-04-27 18:31:51 +0900

    add : 게시글 임시 반응형 & 상세 페이지 CSS

--- frontend/src/pages/Board/BoardDetail.jsx
+++ frontend/src/pages/Board/BoardDetail.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Axios } from '../../core/axios';
 import { useParams } from 'react-router-dom';
-
+import sample from '../../assets/images/Initimage.PNG';
 import './BoardDetail.scss';
 
 export const BoardDetail = () => {
@@ -17,10 +17,40 @@ export const BoardDetail = () => {
 
   return (
     <>
-      <h1>{id}번 상세페이지</h1>
       <section className="container">
         <div className="detailFormWrap">
-          <button>test</button>
+          <div className="detailHeader row">
+            <div className="col-6 d-flex">
+              <img
+                className="detailProfileImg"
+                src="/img/basicProfile.png"
+                alt="이미지 없음..."></img>
+              <p className="detailUserName">김아무개</p>
+            </div>
+            <div className="detailStatus col-6">거래 완료</div>
+          </div>
+          <div className="detailBody row">
+            <div className="detailIngredientImg col-5">
+              <img src={sample}></img>
+            </div>
+            <div className="detailContent col-7">
+              <div className="detailTitle">고구마 나눔합니다.</div>
+              <div className="detailInfo">
+                <div>고구마</div>
+                <div>구매일/유통기한 : 2022-04-11</div>
+                <div>4km </div>
+              </div>
+              <div className="detailDescription">
+                고구마 나눔합니다 *^^* 필요하신 분 채팅주세요.
+              </div>
+              <div className="detailIcon">댓글아이콘 좋아요아이콘 신고아이콘</div>
+            </div>
+          </div>
+          <div className="detailFooter">
+            <button id="detailCancelBtn">취소</button>
+            <button id="detailActiveBtn">채팅하기</button>
+            <button id="detailActiveBtn">수정</button>
+          </div>
         </div>
       </section>
     </>
