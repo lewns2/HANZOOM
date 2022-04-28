5a75438647b4a701b2c4693282d25b4904282f87
zlewns <zlewns@gmail.com>
2022-04-29 00:15:21 +0900

    update : 게시판 검색 요청, 필터 요청, 글쓰기

--- frontend/src/components/Board/SearchBar.jsx
+++ frontend/src/components/Board/SearchBar.jsx
@@ -1,12 +1,20 @@
-export const SearchBar = () => {
+import { useState, useEffect } from 'react';
+
+export const SearchBar = (props) => {
+  const [keyword, setKeyWord] = useState();
   return (
     <div className="searchBar d-flex justify-content-end px-4">
       <input
         className="form-control mx-1"
         id="text-area"
         type="text"
-        placeholder="찾으시는 식재료명을 입력해주세요"></input>
-      <button id="searchBtn">검색</button>
+        placeholder="찾으시는 식재료명을 입력해주세요"
+        onChange={(e) => {
+          setKeyWord(e.target.value);
+        }}></input>
+      <button id="searchBtn" onClick={() => props.setSearchKeyword(keyword)}>
+        검색
+      </button>
     </div>
   );
 };
