eb24ea3bc9b4b7a892866504c9760fd4142a35b3
김동주 <rlaehdwn23@naver.com>
2022-05-03 14:11:54 +0900

    update : user's favorite list
    - 찜 목록 더미 데이터에서 실제 데이터로 변경

--- frontend/src/components/User/MyPage/FavoriteList.jsx
+++ frontend/src/components/User/MyPage/FavoriteList.jsx
@@ -2,18 +2,41 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { settings } from '../../../constants/slider';
+import { useEffect, useState } from 'react';
+import { Axios } from '../../../core/axios';
+import { axios_apis } from '../../../core/axios';
+
+import { FavoriteListItem } from './FavoriteListItem';
 
 export const FavoriteList = () => {
+  const [likeList, setLikeList] = useState();
+
+  const getLikeList = () => {
+    const token = sessionStorage.getItem('jwt-token');
+    Axios.get(`${axios_apis.users.user}/likeList`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((res) => {
+      console.log(res.data);
+      setLikeList(res.data);
+    });
+  };
+
+  useEffect(() => {
+    getLikeList();
+  }, []);
+
   return (
     <>
       <div className="container favoriteList">
-        <Slider {...settings}>
-          <div className="box">test</div>
-          <div className="box">test</div>
-          <div className="box">test</div>
-          <div className="box">test</div>
-          <div className="box">test</div>
-        </Slider>
+        {likeList && (
+          <Slider {...settings}>
+            {likeList.map((like, index) => (
+              <FavoriteListItem key={index} like={like} />
+            ))}
+          </Slider>
+        )}
       </div>
     </>
   );
