a708967906ac30de88db82f4d4ec4ca65a18738e
최영진 <cyjcmb98@naver.com>
2022-05-09 14:50:51 +0900

    add : select matching algorithm

---
+++ backend/src/main/java/com/cdp/hanzoom/api/service/MatchingService.java
@@ -0,0 +1,10 @@
+package com.cdp.hanzoom.api.service;
+
+import com.cdp.hanzoom.api.response.BoardFindRes;
+import com.cdp.hanzoom.api.response.MatchingRes;
+
+import java.util.List;
+
+public interface MatchingService {
+    MatchingRes findMatchingList(String userEmail, List<String> ingredients, Double distance);
+}
