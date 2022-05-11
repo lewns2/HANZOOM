60c4a23183958838f3451721eebcc8faa19f603c
최영진 <cyjcmb98@naver.com>
2022-05-11 18:17:08 +0900

    add : admin pendingIngredient

--- frontend/src/pages/Admin/Admin.jsx
+++ frontend/src/pages/Admin/Admin.jsx
@@ -1,12 +1,42 @@
 import { useEffect, useState } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import { Report } from '../../components/Admin/Report';
+import { PendingIngredient } from '../../components/Admin/PendingIngredient';
+
+import './Admin.scss';
 
 export const Admin = () => {
+  const [selectedType, setSelectedType] = useState('신고내역');
+
+  const handleChange = (e) => {
+    // console.log(e.target.value);
+    setSelectedType(e.target.value);
+  };
+
   return (
     <>
       <Container>
-        <Report />
+        <div className="matchFilterContainer">
+          <div className="tabs">
+            <input type="radio" id="radio-1" name="tabs" value="신고내역" onChange={handleChange} />
+            <label className="tab" htmlFor="radio-1">
+              신고내역
+            </label>
+            <input
+              type="radio"
+              id="radio-2"
+              name="tabs"
+              value="식재료요청"
+              onChange={handleChange}
+            />
+            <label className="tab" htmlFor="radio-2">
+              식재료 등록 요청
+            </label>
+            <span className="glider"></span>
+          </div>
+        </div>
+        {selectedType === '신고내역' && <Report />}
+        {selectedType === '식재료요청' && <PendingIngredient />}
       </Container>
     </>
   );
