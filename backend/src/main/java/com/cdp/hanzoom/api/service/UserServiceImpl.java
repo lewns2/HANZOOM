739e9ba9b5051bc11cd269a7405c76ff7fb464a6
최영진 <cyjcmb98@naver.com>
2022-05-12 15:47:55 +0900

    update : signup reportNumber 0

--- backend/src/main/java/com/cdp/hanzoom/api/service/UserServiceImpl.java
+++ backend/src/main/java/com/cdp/hanzoom/api/service/UserServiceImpl.java
@@ -48,6 +48,7 @@ public class UserServiceImpl implements UserService {
 		user.setUserNickname(userRegisterInfo.getUserNickname());
 		// 보안을 위해서 유저 패스워드 암호화 하여 디비에 저장.
 		user.setUserPassword(passwordEncoder.encode(userRegisterInfo.getUserPassword()));
+		user.setReportedNumber(0);
 		return userRepository.save(user);
 	}
 
