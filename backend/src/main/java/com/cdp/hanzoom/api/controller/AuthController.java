a310a23f1a88c4ce75052e6b029f1df88e83bbd2
한성희 <bee1404@naver.com>
2022-05-12 11:58:27 +0900

    add : check user's reported number when user log in
    - 유저 로그인 시, 신고 누적 횟수 검사
    - 신고 누적 횟수 3회 이상 시, 406 에러 return
    - 관리자 신고 접수 거절 시, 유저 누적 신고수 감소 이슈 제거

--- backend/src/main/java/com/cdp/hanzoom/api/controller/AuthController.java
+++ backend/src/main/java/com/cdp/hanzoom/api/controller/AuthController.java
@@ -6,6 +6,9 @@ import com.cdp.hanzoom.api.request.UserRegisterReq;
 import com.cdp.hanzoom.api.service.KaKaoService;
 import com.cdp.hanzoom.api.service.UserService;
 import com.cdp.hanzoom.db.entity.User;
+import com.cdp.hanzoom.db.entity.UserReportHistory;
+import com.cdp.hanzoom.db.repository.UserRepository;
+import com.cdp.hanzoom.db.repository.UserRepositorySupport;
 import org.joda.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -43,6 +46,8 @@ public class AuthController {
     KaKaoService kaKaoService;
     @Autowired
     PasswordEncoder passwordEncoder;
+    @Autowired
+    UserRepositorySupport userRepositorySupport;
 
     @PostMapping("/login")
     @ApiOperation(value = "로그인", notes = "<strong>아이디와 패스워드</strong>를 통해 로그인 한다.")
@@ -50,6 +55,7 @@ public class AuthController {
             @ApiResponse(code = 200, message = "성공", response = UserLoginRes.class),
             @ApiResponse(code = 401, message = "인증 실패", response = BaseResponseBody.class),
             @ApiResponse(code = 404, message = "사용자 없음", response = BaseResponseBody.class),
+            @ApiResponse(code = 406, message = "허용되지 않음", response = BaseResponseBody.class),
             @ApiResponse(code = 500, message = "서버 오류", response = BaseResponseBody.class)
     })
     public ResponseEntity<UserLoginRes> login(@RequestBody @ApiParam(value="로그인 정보", required = true) UserLoginReq loginInfo) {
@@ -59,9 +65,14 @@ public class AuthController {
         User user = userService.getUserByUserEmail(userEmail);
         // 로그인 요청한 유저로부터 입력된 패스워드 와 디비에 저장된 유저의 암호화된 패스워드가 같은지 확인.(유효한 패스워드인지 여부 확인)
         if(passwordEncoder.matches(userPassword, user.getUserPassword())) {
+            // 해당 유저의 신고 누적횟수가 3회 이상이면, 이용 제한
+            if(userRepositorySupport.findReportedNumber(user.getUserEmail()) > 2) {
+                return ResponseEntity.status(406).body(UserLoginRes.of(406, "Banned User", null));
+            }
             // 유효한 패스워드가 맞는 경우, 로그인 성공으로 응답.(액세스 토큰을 포함하여 응답값 전달)
             return ResponseEntity.ok(UserLoginRes.of(200, "Success", JwtTokenUtil.getToken(userEmail)));
         }
+
         // 유효하지 않는 패스워드인 경우, 로그인 실패로 응답.
         return ResponseEntity.status(401).body(UserLoginRes.of(401, "Invalid Password", null));
     }
@@ -97,6 +108,11 @@ public class AuthController {
             //임의로 리턴된 User 인스턴스. 현재 코드는 회원 가입 성공 여부만 판단하기 때문에 굳이 Insert 된 유저 정보를 응답하지 않음.
             user = kaKaoService.registerUser(registerInfo);
         }
+
+        if(userRepositorySupport.findReportedNumber(user.getUserEmail()) > 2) {
+            return ResponseEntity.status(406).body(UserLoginRes.of(406, "Banned User", null));
+        }
+
         // 회원 가입이 되어 있다면, 로그인 성공으로 응답.(액세스 토큰을 포함하여 응답값 전달)
         return ResponseEntity.ok(UserLoginRes.of(200, "Success", JwtTokenUtil.getToken(user.getUserEmail())));
     }
