d874503785f84866ed494609b8235da3ebc213be
yoonjung1205 <yoonjung1205@naver.com>
2022-05-14 01:25:06 +0900

    update : drag and drop hover event - 드래그앤드랍 호버시 아이콘 나타나기

--- frontend/src/pages/User/MyFoodIngredients/ModiMyFoodIngredients.jsx
+++ frontend/src/pages/User/MyFoodIngredients/ModiMyFoodIngredients.jsx
@@ -1,9 +1,18 @@
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import HelpOutlineRoundedIcon from '@mui/icons-material/HelpOutlineRounded';
 import { useEffect, useState } from 'react';
 import { Calendar } from '../../../components/Board/Calendar';
 import { Axios } from '../../../core/axios.js';
 import { AutoComplete } from './AutoComplete';
+
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import HelpOutlineRoundedIcon from '@mui/icons-material/HelpOutlineRounded';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 export const ModiMyFoodIngredients = (props) => {
   const [ingreName, setIngreName] = useState();
@@ -21,6 +30,53 @@ export const ModiMyFoodIngredients = (props) => {
         console.log(err);
       });
   };
+
+  function createData(name, expiration) {
+    return { name, expiration };
+  }
+
+  const rows = [
+    createData('신선식품(농수산물)', '7일 이내'),
+    createData('장류(고추장,된장 등)', '18개월 이내'),
+  ];
+
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: '#f7c343',
+      color: theme.palette.common.black,
+      fontWeight: 'bold',
+      fontFamily: 'GmarketSansBold',
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontFamily: 'GmarketSansMedium',
+    },
+  }));
+
+  const BasicTable = () => {
+    return (
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow sx={{ fontWeight: 'bold' }}>
+              <StyledTableCell>식품</StyledTableCell>
+              <StyledTableCell align="right">유통기한</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <StyledTableCell component="th" scope="row">
+                  {row.name}
+                </StyledTableCell>
+                <StyledTableCell align="right">{row.expiration}</StyledTableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  };
+
   useEffect(() => {
     getIngreInfo();
   }, []);
@@ -61,10 +117,21 @@ export const ModiMyFoodIngredients = (props) => {
         <div>
           유통기한
           <CalendarMonthIcon />
+          <a
+            data-bs-toggle="collapse"
+            href="#collapseExample"
+            aria-expanded="false"
+            aria-controls="collapseExample">
+            <HelpOutlineRoundedIcon />
+          </a>
+          <div className="collapse" id="collapseExample">
+            <div className="p-3">
+              <BasicTable />
+            </div>
+          </div>
           {modiExpirationDate && (
             <Calendar setSelectedDate={setModiExpirationDate} originalDate={modiExpirationDate} />
           )}
-          <HelpOutlineRoundedIcon />
         </div>
       </div>
     </div>
