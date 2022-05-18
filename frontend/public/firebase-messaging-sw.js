42c290d2969deddecaf238ee365398ca05a95db4
zlewns <zlewns@gmail.com>
2022-05-19 00:58:38 +0900

    임시 저장

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -15,20 +15,38 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-//백그라운드 서비스워커 설정
+// firebase 백그라운드 서비스워커 설정 비활성화
 
-self.addEventListener('push', function (event) {
-  const payload = event.data.json();
-  const title = payload.notification.title;
-  const options = {
-    body: payload.notification.body,
-    icon: payload.notification.icon,
-    data: payload.notification.click_action,
-    icon: '/favicon.ico',
-  };
-  event.waitUntil(self.registration.showNotification(title, options));
-});
-self.addEventListener('notificationclick', function (event) {
-  event.notification.close();
-  event.waitUntil(clients.openWindow(event.notification.data));
-});
+// self.addEventListener('push', function (event) {
+//   const payload = event.data.json();
+//   console.log(payload);
+//   const title = payload.data.google.c.a.c_l;
+//   const options = {
+//     // body: payload.notification.body,
+//     // icon: payload.notification.icon,
+//     // data: payload.notification.click_action,
+//     icon: '/favicon.ico',
+//   };
+//   event.waitUntil(self.registration.showNotification(title, options));
+// });
+// self.addEventListener('notificationclick', function (event) {
+//   event.notification.close();
+//   event.waitUntil(clients.openWindow(event.notification.data));
+// });
+
+// self.addEventListener('push', function (event) {
+//   const payload = event.data.json();
+//   console.log(payload);
+//   const title = payload.notification.title;
+//   const options = {
+//     body: payload.notification.body,
+//     icon: payload.notification.icon,
+//     data: payload.notification.click_action,
+//     icon: '/favicon.ico',
+//   };
+//   event.waitUntil(self.registration.showNotification(title, options));
+// });
+// self.addEventListener('notificationclick', function (event) {
+//   event.notification.close();
+//   event.waitUntil(clients.openWindow(event.notification.data));
+// });
